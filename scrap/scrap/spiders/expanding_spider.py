1557c1cc58f9defe957b1be1756ef7ad5132768a
DTomusk <david.tomusk@gmail.com>
2020-06-22 08:39:34 +0100

    New spider, mostly copied but cleaner and with a different direction

---
+++ scrap/scrap/spiders/expanding_spider.py
@@ -0,0 +1,58 @@
+import scrapy 
+import os.path
+import re
+from graph import Graph
+from scrapy import signals
+from scrapy import Spider
+
+# this spider looks at one film or person at a time, it can be called multiple times to grow the graph
+class GrowSpider(scrapy.Spider):
+	name = "grow"
+	filename = "database.json"
+
+	def start_requests(self):
+		# if file exists append if not create 
+		if os.path.isfile(self.filename):
+			# find the entry corresponding to the cl arguments 
+			pass
+		else:
+			# creates a new graph and parses the first actor 
+			self.graph = Graph()
+			yield scrapy.Request('http://m.imdb.com/find?q=%s+%s&ref_=nv_sr_sm' % (self.arg1, self.arg2))
+
+	# parse is the initial step, it searches for the first actor and yields their page 
+	def parse(self, response):
+		name_extension = response.xpath('//a[contains(@href,"/name/nm")]/@href').get()
+		name = re.sub('\n', '', response.xpath('//div[@class="media-body media-vertical-align"]/span[@class="h3"]/text()').get())
+		code = re.sub('name/', '', name_extension.strip('/'))
+		self.graph.new_actor(code, name)
+		yield scrapy.Request('http://m.imdb.com%s' % name_extension, callback=self.parse_actor, meta={'code':code})
+
+	# parses the page for a given actor by adding their top movies to their entry and adding them to the entry of the movie 
+	def parse_actor(self, response):
+		# extensions are what's added to the url to get a certain film
+		title_extensions = response.xpath('//div[@class="text-center filmo-caption"]/small[@class="ellipse"]/a[@href]/@href').getall()
+		titles = response.xpath('//div[@class="text-center filmo-caption"]/small[@class="ellipse"]/a[@href]/text()').getall()
+
+		for i, title_extension in enumerate(title_extensions):
+			title_code = re.sub('title/', '', title_extension.strip('/'))
+			title = re.sub('\n', '', titles[i].strip())
+			self.graph.new_film(title_code, title)
+			self.graph.add_film_to_actor(response.meta['code'], title_code)
+			self.graph.add_actor_to_film(title_code, response.meta['code'])
+			
+	def parse_film(self, response):
+		pass
+
+	# copied from documentation 
+	@classmethod
+	def from_crawler(cls, crawler, *args, **kwargs):
+		spider = super(GrowSpider, cls).from_crawler(crawler, *args, **kwargs)
+		crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
+		return spider
+
+	# saves graph to file when spider closes 
+	def spider_closed(self, spider):
+		spider.logger.info('Spider closed')
+		self.graph.write_graph_to_file(self.filename)
+		#plot.plot_graph(self.graph)
