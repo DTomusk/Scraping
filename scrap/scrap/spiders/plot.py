73573c7f65116d48d85914ccbd21455f5733ab6a
DTomusk <david.tomusk@gmail.com>
2020-06-24 10:09:19 +0100

    Fixed the colors, now films are one color and actors are another

--- scrap/scrap/spiders/plot.py
+++ scrap/scrap/spiders/plot.py
@@ -3,14 +3,12 @@ import plotly.graph_objects as go
 
 def plot_graph(data):
 	G = nx.Graph()
-	colors = []
+	
 	# needs to be a condition for adding film nodes, such as rating or popularity 
 	for actor in data.actors:
-		G.add_node(data.actors[actor]["name"])
-		colors.append("rgb(0, 0, 240)")
+		G.add_node(data.actors[actor]["name"], types='actor')
 		for film in data.actors[actor]["films"]:
-			G.add_node(data.films[film]["title"])
-			colors.append("rgb(240, 0, 0)")
+			G.add_node(data.films[film]["title"], types='film')
 			G.add_edge(data.actors[actor]["name"], data.films[film]["title"])
 
 	pos = nx.spring_layout(G)
@@ -33,12 +31,18 @@ def plot_graph(data):
 	node_x = []
 	node_y = []
 	names = []
+	colors = []
 
 	for node in G.nodes():
 		x, y = pos[node]
 		node_x.append(x)
 		node_y.append(y)
 		names.append(node)
+		# check color attribute of node to assign the right colour 
+		if G.node[node]['types'] == 'actor':
+			colors.append("rgb(0, 0, 240)")
+		else:
+			colors.append("rgb(240, 0, 0)")
 
 	# set hover info to be name or title 
 	node_trace = go.Scatter(x=node_x, y=node_y, mode='markers', marker=dict(size=10, color=colors), hovertext=names)
